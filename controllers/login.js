0add70a704633098922ff8c8677f1d9733c619e0
solomon stephen <33923021+realrufans@users.noreply.github.com>
2024-06-02 18:42:43 +0100

    now requires username and password to login

--- controllers/login.js
+++ controllers/login.js
@@ -1,42 +1,46 @@
 const express = require("express");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
- 
+
 const { StatusCodes } = require("http-status-codes");
 const adminModel = require("../models/admin.model");
 const staffModel = require("../models/staff.model");
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    throw new BadRequestError("Please provide email and password!");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new BadRequestError("Please provide username and password!");
   }
-  let user;
+  try {
+    let user;
 
-  // Check if the user is a farm worker
-  user = await staffModel.findOne({ email });
-  if (!user) {
-    // If not, check if the user is an admin
-    user = await adminModel.findOne({ email });
+    // Check if the user is a farm worker
+    user = await staffModel.findOne({ username });
     if (!user) {
-      // If neither, throw an error
+      // If not, check if the user is an admin
+      user = await adminModel.findOne({ username });
+      if (!user) {
+        // If neither, throw an error
+        throw new UnauthenticatedError("Invalid credentials");
+      }
+    }
+
+    // If user is found, validate the password
+
+    const isValidPassword = await user.comparePassword(password);
+
+    if (!isValidPassword) {
       throw new UnauthenticatedError("Invalid credentials");
     }
+
+    // If password is valid, generate JWT token and respond
+    const name = user.username;
+    const { isAdmin } = user;
+    const token = user.createJwt();
+
+    res.status(StatusCodes.OK).json({ isAdmin, username: name, token });
+  } catch (error) {
+    return res.status(StatusCodes.UNAUTHORIZED).json(error);
   }
-
-  // If user is found, validate the password
-
-  const isValidPassword = await user.comparePassword(password);
-
-  if (!isValidPassword) {
-    throw new UnauthenticatedError("Invalid credentials");
-  }
-
-  // If password is valid, generate JWT token and respond
-  const name = user.username;
-  const { isAdmin } = user;
-  const token = user.createJwt();
-
-  res.status(StatusCodes.OK).json({ isAdmin, username: name, token });
 };
 
 module.exports = login;
